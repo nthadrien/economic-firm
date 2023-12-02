0047eaab7d0af065b9483c6e85cef47b787e1c39
nthadrien <hadrien.wakacode@gmail.com>
2023-12-02 03:23:09 +0300

    designs changes

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 window.onload = () => {
     console.log('windows loaded')
-};
 
-const btn = document.querySelector('#hamburger');
-const nav = document.querySelector('.nav-links');
-btn.addEventListener('click', () => {
-    nav.classList.toggle('show');
-    btn.classList.toggle('open');
-});
 
-function showIt ( e ) {
-    console.log(e.target)
+    const boxes = document.querySelectorAll(".inter");
+    const cards = document.querySelectorAll(".card");
+
+    const btn = document.querySelector('#hamburger');
+    const nav = document.querySelector('.nav-links');
+    btn.addEventListener('click', () => {
+        nav.classList.toggle('show');
+        btn.classList.toggle('open');
+    });
+
+    let options = {
+        rootMargin: window.innerWidth > 768 ? "-200px" : "-100px",
+    };
+
+    let callback = (entries, observer) => {
+        entries.forEach((entry) => {
+            entry.target.classList.toggle("animate", entry.isIntersecting );
+            if ( entry.isIntersecting ) observer.unobserve(entry.target);
+        });
+    };
+
+    let observer = new IntersectionObserver(callback, options);
+
+    boxes.forEach( card => {
+        observer.observe( card );
+    });
 };
